2cc4563dff0d2e56472ebe0a7ebe9a674bd0f1b8
Pontus Lurcock <pont@talvi.net>
2020-11-15 11:45:18 +0100

    Add some type annotations

--- xml-to-github.py
+++ xml-to-github.py
@@ -1,29 +1,33 @@
 #!/usr/bin/python3
 
-import sys
-from bs4 import BeautifulSoup
+from __future__ import annotations  # see PEP 563
+
 import datetime
+import sys
+from typing import Mapping, List
+
+from bs4 import BeautifulSoup, PageElement, Tag, ResultSet
 
 
 def main():
     with open(sys.argv[1], "r") as fh:
         file_content = fh.read()
     soup = BeautifulSoup(file_content, "xml")
-    bugs = soup.bugs.find_all("bug")
-    target_map = extract_targets(bugs)
-    bug_list = convert_bugs(bugs, target_map)
+    bug_tags = soup.bugs.find_all("bug")
+    target_map = extract_targets(bug_tags)
+    bug_list = convert_bugs(bug_tags, target_map)
     for bug in bug_list:
         print(bug.summary())
     print("%d bugs read" % len(bug_list))
 
 
-def extract_targets(bug_tags):
+def extract_targets(bug_tags: ResultSet) -> Mapping[str, Target]:
     """Convert BeautifulSoup ResultSet to a targets dictionary
 
     Take a BeautifulSoup ResultSet containing exported Bugs Everywhere
     bugs, included targets represented as bugs.
 
-    Return a dictionary mapping target UUIDs to Target objects.
+    Return a dictionary mapping target UUID strings to Target objects.
     """
 
     target_map = {}
@@ -35,7 +39,9 @@ def extract_targets(bug_tags):
     return target_map
 
 
-def convert_bugs(be_bugs, target_map):
+def convert_bugs(
+        bug_tags: ResultSet, target_map: Mapping[str, Target]
+) -> List[Bug]:
     """Convert BeautifulSoup ResultSet to a list of bugs
 
     Take a BeautifulSoup ResultSet containing exported Bugs Everywhere
@@ -45,17 +51,17 @@ def convert_bugs(be_bugs, target_map):
     """
 
     bug_list = []
-    for be_bug in be_bugs:
-        severity = be_bug.severity.get_text()
+    for bug_tag in bug_tags:
+        severity = bug_tag.severity.get_text()
         if severity != "target":
-            bug_list.append(Bug(be_bug, target_map))
+            bug_list.append(Bug(bug_tag, target_map))
     
     return sorted(bug_list, key=lambda b: b.created_at)
 
 
 class Bug:
 
-    def __init__(self, soup_tag, target_map):
+    def __init__(self, soup_tag: Tag, target_map: Mapping[str, Target]):
         self.labels = []
         be_status = soup_tag.status.get_text()
         self.state = "open" if be_status == "open" else "closed"
@@ -84,7 +90,7 @@ class Bug:
                 if uuid in target_map:
                     self.milestone = target_map[uuid]
 
-    def summary(self):
+    def summary(self) -> str:
         bug_line = self.created_at.strftime("%Y-%m-%d ") + str(self.milestone)\
                    + " " + self.state[0] + " " + self.title
         comment_lines = ["\n  " + c.created_at.strftime("%Y-%m-%d ")
@@ -95,14 +101,14 @@ class Bug:
 
 class Comment:
 
-    def __init__(self, soup_tag):
+    def __init__(self, soup_tag: Tag):
         self.created_at = get_be_creation_date(soup_tag, "date")
         self.body_text = soup_tag.body.get_text()
 
 
 class Target:
 
-    def __init__(self, soup_tag):
+    def __init__(self, soup_tag: Tag):
         self.title = soup_tag.summary.get_text()
         self.created_at = get_be_creation_date(soup_tag)
         self.closed = not (soup_tag.status.get_text() == "open")
@@ -111,7 +117,7 @@ class Target:
         return self.title
 
 
-def get_be_creation_date(soup_tag, subtag_name="created"):
+def get_be_creation_date(soup_tag: Tag, subtag_name: str = "created"):
     # Date format is RFC2822, e.g. "Wed, 01 Apr 2009 22:12:16 +0000".
     # Timezone specifier is always +0000.
     return datetime.datetime.strptime(soup_tag.find(subtag_name).get_text(),
