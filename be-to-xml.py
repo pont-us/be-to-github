1fdf8c220b5d1293ae8b0328d72cbaf30d17694b
Pontus Lurcock <pont@talvi.net>
2020-11-15 15:35:11 +0100

    Add readme, license, and copyright notices

--- be-to-xml.py
+++ be-to-xml.py
@@ -1,9 +1,27 @@
 #!/usr/bin/python2
 
+# Copyright 2020 Pontus Lurcock
+#
+# This file is part of be-to-github.
+#
+# be-to-github is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# be-to-github is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with be-to-github. If not, see <https://www.gnu.org/licenses/>.
+
 import libbe
 import libbe.storage.base
 import libbe.storage.vcs.git
 import libbe.bugdir
+
 
 def main():
     storage = libbe.storage.vcs.git.ExecGit(".")
