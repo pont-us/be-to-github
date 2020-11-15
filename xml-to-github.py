0e5e2814871daa44959a4c01a1815253f9cb5ee3
Pontus Lurcock <pont@talvi.net>
2020-11-15 15:15:02 +0100

    Implement proof-of-concept export functionality
    xml-to-github.py can now export a single bug (with title and body text
    only, and no comments or milestones) to a GitHub issue.

--- xml-to-github.py
+++ xml-to-github.py
@@ -4,6 +4,8 @@ from __future__ import annotations  # see PEP 563
 
 import argparse
 import datetime
+import requests
+import os
 from typing import Mapping, List, Optional
 
 from bs4 import BeautifulSoup, Tag, ResultSet
@@ -13,6 +15,10 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--dump", action="store_true",
                         help="Write a summary to the standard output.")
+    parser.add_argument("--owner", type=str,
+                        help="GitHub username of repository owner")
+    parser.add_argument("--repo", type=str,
+                        help="name of GitHub repository")
     parser.add_argument("xml_file", type=str, help="exported BE XML file")
     args = parser.parse_args()
     with open(args.xml_file, "r") as fh:
@@ -20,6 +26,8 @@ def main():
     converter = Converter(file_content)
     if args.dump:
         converter.print_summary()
+    if args.owner and args.repo:
+        converter.export_to_github(args.owner, args.repo)
 
 
 class Converter:
@@ -28,12 +36,40 @@ class Converter:
         soup = BeautifulSoup(file_content, "xml")
         bug_tags = soup.bugs.find_all("bug")
         target_map = self.extract_targets(bug_tags)
+        self._graphql_headers = \
+            {"Authorization": "Bearer " + os.environ["BE_TO_GITHUB_TOKEN"]}
         self.bug_list = self.convert_bugs(bug_tags, target_map)
 
     def print_summary(self):
         for bug in self.bug_list:
             print(bug.summary())
         print("%d bugs read" % len(self.bug_list))
+
+    def export_to_github(self, owner: str, repo_name: str):
+        # TODO: implement fully -- at present, this only converts one bug.
+        repo_id = self._get_repo_id(owner, repo_name)
+        self._graphql_query(self.bug_list[0].to_graphql(repo_id))
+
+    def _get_repo_id(self, owner, repo_name):
+        query = """
+            query Operation1 {
+                repository(owner: "%s", name: "%s") {
+                    id
+                }
+            }
+        """ % (owner, repo_name)
+        result = self._graphql_query(query)
+        return result["data"]["repository"]["id"]
+
+    def _graphql_query(self, query: str) -> Mapping:
+        request = requests.post(
+            "https://api.github.com/graphql",
+            json={"query": query}, headers=self._graphql_headers)
+        if request.status_code == 200:
+            return request.json()
+        else:
+            raise Exception(f"GraphQL query failed "
+                            f"(Status code: {request.status_code})")
 
     @staticmethod
     def extract_targets(bug_tags: ResultSet) -> Mapping[str, Target]:
@@ -123,6 +159,24 @@ class Bug:
                          + c.body_text.translate({ord("\n"): ord(" ")})[:60]
                          for c in self.comments]
         return bug_line + "".join(comment_lines)
+
+    def to_graphql(self, repo_id: str) -> str:
+        # TODO: implement fully.
+        # Currently only includes title and body, and doesn't add comments.
+        # (But comments may need a separate method anyway, since we don't
+        # know the Issue ID until it's been created.)
+        query = """
+        mutation Operation1 {
+            createIssue(input: {
+                repositoryId: "%s",
+                title: "%s",
+                body: "%s"}) {
+                issue {
+                    title, body
+                }
+            }
+        }""" % (repo_id, self.title, self.body)
+        return query
 
 
 class Comment:
